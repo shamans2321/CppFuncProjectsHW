6f574e62880b70366c7cf0aa2b2f64126b332bd1
Никита Паутов <145455384+shamans2321@users.noreply.github.com>
2023-10-25 16:34:50 +0300

    Add files via upload
    домашняя работа 1
    Написать программу вычиляющую площадь круга, длину окружности и
    диаметр по введенному радиусу
    Написать программу вычисляющую итоговую оценку студента за год.
    Пользователь вводит итоговую оценку студента за каждую четверть.

---
+++ дз1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int R, S, C, a, b, c;
+    float P;
+    P = 3.14;
+    cout << "введите радиус:";
+    cin >> R;
+    S = P * (R * R);
+    C = 2 * P * R;
+    cout << S << endl;
+    cout << C<< endl;
+
+
+    int ch1, ch2, ch3, ch4, gd, sum;
+    cout << "введите оценки за четверть:";
+    cin >> ch1;
+    cin >> ch2;
+    cin >> ch3;
+    cin >> ch4;
+    sum = ch1 + ch2 + ch3 + ch4;
+    cout << sum / 4;
+
+}
