b4ba7a15b158477756abb844c44fa7804851a7b9
Никита Паутов <145455384+shamans2321@users.noreply.github.com>
2023-10-25 16:48:21 +0300

    домашняя работа 3
    Пользователь вводит с клавиатуры символ. Определить,
    какой это символ: Буква, цифра, знак препинания или другое.
    Вася работает программистом и получает 50$ за каждые
    штрафуют на 20$. Реализовать меню:
    - пользователь вводит желаемый доход Васи и количество
    опозданий, посчитать, сколько строк кода емунадонаписать;
    - пользователь вводит количество строк кода, написанное
    Васей и желаемый объем зарплаты. Посчитать, сколько
    раз Вася может опоздать;
    2
    - пользователь вводит количество строк кода и количество
    опозданий, определить, сколько денег заплатят Васе и
    заплатят ли вообще.

---
+++ дз3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{//1
+	setlocale(LC_ALL, "RU");
+	char a;
+	cin >> a;
+	if (a >= '0' && a <= '9') {
+		cout << "цыфра";
+	}
+	else if (a >= 'A' && a <= 'Z') {
+		cout << "буква";
+	}
+	else if (a >= 'a' && a <= 'z') {
+		cout << "буква";
+	}
+	else if ((a == '.') or (a == '!') or (a == '?') or (a == ',')) {
+		cout << "знак препинания";
+	}
+
+
+//3
+	int doh, op, st;
+	cout << "введите доход и количество опозданий" << endl;
+	cin >> doh >> op;
+	st = (doh / 50) * 100;
+	cout << st;
+
+	cout << "введите количество строк и зарплату" << endl;
+	cin >> st >> doh;
+	op = doh / 20;
+	cout << op<< "количество опозданий";
+	cout << "введите количество строк и опозданий" << endl;
+	cin >> st >> op;
+	doh = (st / 50) * 100;
+	op = op / 3;
+	doh = doh / op;
+	if (doh > 0) {
+		cout << "вася получит:" << doh;
+	}
+	else (cout << "васе не заплатят");
+	
+}
